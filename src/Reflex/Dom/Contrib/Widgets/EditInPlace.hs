da6a666677326a5310cd5e2037bfc0e6398a0667
koenigmaximilian <github@koenigmaximilian.de>
2018-02-20 23:14:32 +0000

    Update EditInPlace.hs

--- src/Reflex/Dom/Contrib/Widgets/EditInPlace.hs
+++ src/Reflex/Dom/Contrib/Widgets/EditInPlace.hs
@@ -26,7 +26,7 @@ import           Reflex
 import           Reflex.Dom.Core
 import           Reflex.Dom.Contrib.Utils
 import           Reflex.Dom.Contrib.Widgets.Common
-import           GHCJS.DOM.Types (MonadJSM)
+import           GHCJS.DOM.Types (MonadJSM, liftJSM)
 ------------------------------------------------------------------------------
 
 
@@ -109,7 +109,7 @@ editor name = do
   (e,w) <- htmlTextInput' "text" $ WidgetConfig
     (tagPromptlyDyn name pb) "" (constDyn mempty)
   performEvent_ $ ffor pb $ \_ -> do
-    liftIO $ focus e
+    liftJSM $ focus e
   let acceptEvent = leftmost
         [ () <$ ffilter (==13) (_hwidget_keypress w)
         , () <$ ffilter not (updated $ _hwidget_hasFocus w)
